1c22fac5c0a03a15972ae8debfd8c5d218a2f0b5
wgengland <wgengland@LAP-060.navtechradar.co.uk>
2021-05-02 23:15:55 +0100

    Co-routines added 2

--- conc_GO/conc_GO.go
+++ conc_GO/conc_GO.go
@@ -15,11 +15,11 @@ func main() {
 	}
 	fmt.Println(msg)
 	go count("sheep", 1000)
-	count("fish", 100)
+	count("fish", 200)
 }
 
 func count(thing string, delay time.Duration) {
-	for i := 1; true; i++ {
+	for i := 0; i < 10; i++ {
 		fmt.Println(i, thing)
 		time.Sleep(time.Millisecond * delay)
 	}
