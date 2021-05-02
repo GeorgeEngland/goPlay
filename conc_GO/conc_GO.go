8c61cdf3e20b8e99e45d0a900b24c9fe631b0a8e
wgengland <wgengland@LAP-060.navtechradar.co.uk>
2021-05-02 23:12:29 +0100

    Co-routines added

--- conc_GO/conc_GO.go
+++ conc_GO/conc_GO.go
@@ -3,15 +3,24 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"play.com/greetings_GO"
 )
 
 func main() {
-	fmt.Println("testing conc")
 	msg, err := greetings_GO.Hello("George")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Println(msg)
+	go count("sheep", 1000)
+	count("fish", 100)
+}
+
+func count(thing string, delay time.Duration) {
+	for i := 1; true; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * delay)
+	}
 }
